bd82cdece9fae57f877990ab2b2425ba03aeb935
Binozo <70137898+Binozo@users.noreply.github.com>
2022-10-07 18:59:44 +0200

    added code to main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"github.com/instantminecraft/client/pkg/auth"
+	"github.com/instantminecraft/client/pkg/proxy"
+	"github.com/instantminecraft/client/pkg/router"
+	"github.com/instantminecraft/client/pkg/server"
+	"log"
+)
 
 func main() {
-	fmt.Println("Hello :)")
+	if !auth.HasAuthKey() {
+		log.Println("Warning: \"auth\" environment variable has not been set. The http server will start without authentication.")
+	}
+	routes := router.Register()
+	go server.Handle(routes)
+	proxy.Start()
 }
